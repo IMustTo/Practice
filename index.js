7cba7b0bcf2880d6983f024ed93aa94d66f42cd5
IMustTo <cxjbinfo@gmail.com>
2016-11-13 18:08:42 +0800

    static

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.end('Hello World!');
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(process.argv[2]);
